37c7c5b6a4594c139cfc28f7c73902da635c1236
Mauricio <maurytrossero@gmail.com>
2025-08-01 15:04:17 -0300

    rutas

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router'; // <-- cambiás esto
 import LoginView from '../views/LoginView.vue';
 import BudgetView from '@/views/BudgetView.vue';
 import FotoConfirmacionForm from '@/components/pedidos/FotoConfirmacionForm.vue';
@@ -10,13 +10,15 @@ const routes = [
   { path: '/foto-pedidos', component: FotoConfirmacionList, meta: { requiresAuth: true } },
   { path: '/foto-pedidos/nuevo', component: FotoConfirmacionForm, meta: { requiresAuth: true } },
   { path: '/', redirect: '/presupuestos' },
-  { path: '/foto-pedidos/editar',  name: 'EditarPedido',  component: () => import('@/views/EditarPedidoView.vue'),
-}
-
+  {
+    path: '/foto-pedidos/editar',
+    name: 'EditarPedido',
+    component: () => import('@/views/EditarPedidoView.vue'),
+  },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(), // <-- esta línea hace el cambio efectivo
   routes,
 });
 
@@ -24,9 +26,9 @@ router.beforeEach((to, from, next) => {
   const isAuthenticated = localStorage.getItem('token') !== null;
 
   if (to.path === '/login' && isAuthenticated) {
-    next('/presupuestos'); // Ya logueado no ir a login
+    next('/presupuestos');
   } else if (to.meta.requiresAuth && !isAuthenticated) {
-    next('/login'); // Si no está logueado y la ruta requiere auth, ir a login
+    next('/login');
   } else {
     next();
   }
